f078976d8d4fe3434b2408ad1aac947f961e0b96
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-05-07 12:49:53 +0100

    Basic Login, Register, Home routes created and corresponding pages rendered.

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
+app.get("/", (req, res) => {
+	res.render("home")
+})
+app.get("/register", (req, res) => {
+	res.render("register")
+})
+app.get("/login", (req, res) => {
+	res.render("login")
+})
+
 app.listen(4000, () => {
 	console.log("Server started on port 4000.")
 })
